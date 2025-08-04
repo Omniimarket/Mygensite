13a078d66c9658b62bdc36102d4f7012791adb56
omniimarket <omniimarket@gmail.com>
2025-08-04 11:49:27 -0700

    Add AdMaven meta tag to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,10 @@ export default function Document() {
         {/* Favicon - CORRECTED LINK */}
         {/* Your favicon.ico file should be located directly in the /public directory */}
         <link rel="icon" href="/favicon.png" type="image/png" />
-
-        {/* If you have other favicon sizes/types for broader compatibility, you can add them here, e.g.: */}
+          {/* Your AdMaven meta tag goes here */}
+        <meta name='admaven-placement' content='BqdU4rjaG' />
+        {/* Other meta tags, favicons, or external scripts can go here */}
+     {/* If you have other favicon sizes/types for broader compatibility, you can add them here, e.g.: */}
         {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" /> */}
         {/* <link rel="manifest" href="/site.webmanifest" /> */}
       </Head>
